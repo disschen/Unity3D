07e793e66bf33f63fd186ac3ecbf3d74af584e74
disschen <qq1461009741@163.com>
2019-10-09 23:22:18 +0800

    Add files via upload

---
+++ hw5/Scripts/BaseCode.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using mygame;
+
+namespace mygame {
+	
+	public class Director : System.Object {
+		private static Director _instance;
+		public SceneController currentSceneController { get; set; }
+
+		public static Director getInstance() {
+			if (_instance == null) {
+				_instance = new Director ();
+			}
+			return _instance;
+		}
+	}
+
+	public interface SceneController {
+		void LoadResources ();
+	}
+
+	public interface UserAction {
+		void Hit(Vector3 pos);
+		int GetScore();
+		void GameOver();
+		void ReStart();
+		void BeginGame();
+		void FlyUFO();
+	}
+
+	/*-----------------------------------Moveable------------------------------------------*/
+	public class Moveable: MonoBehaviour {
+		
+		int move_speed = 1;
+
+		// change frequently
+		int moving_status;	// 0->not moving, 1->moving to middle, 2->moving to dest
+		Vector3 dest;
+		Vector3 middle;
+
+		void Update() {
+			
+			if (moving_status == 1) {
+				// Debug.Log(transform.position);
+				transform.position = Vector3.MoveTowards (transform.position, dest, move_speed * Time.deltaTime);
+				if (transform.position == dest) {
+					
+					moving_status = 0;
+				}
+			}
+		}
+		public void setDestination(Vector3 _dest,int speed) {
+			move_speed = speed;
+			dest = _dest;
+			moving_status = 1;
+		}
+
+		public void reset() {
+			moving_status = 0;
+		}
+	}
+	
+	public class UFO {
+		public GameObject ufo;
+		public int type;
+		public int speed;
+		public int score = 1;                               //射击此飞碟得分
+		readonly Moveable ms;
+		
+		
+		public UFO(int ufo_type){
+			
+			type = ufo_type;
+			// ufo.transform.scale = scale;
+			Debug.Log("right here UFO 1");
+			if (type == 1) {
+				speed = 5;
+				ufo = Object.Instantiate (Resources.Load ("Prefabs/disk1", typeof(GameObject)), Vector3.zero, Quaternion.identity, null) as GameObject;
+            } else if (type == 2)  {
+				speed = 8;
+				score = 2;
+                ufo = Object.Instantiate (Resources.Load ("Prefabs/disk2", typeof(GameObject)), Vector3.zero, Quaternion.identity, null) as GameObject;
+            } else {
+				speed = 10;
+				score = 3;
+                ufo = Object.Instantiate (Resources.Load ("Prefabs/disk3", typeof(GameObject)), Vector3.zero, Quaternion.identity, null) as GameObject;
+            }
+			ms = ufo.AddComponent(typeof(Moveable)) as Moveable;
+			int fy = Random.Range(-10,10);
+			ufo.transform.position = new Vector3(0,fy,0);
+            // direction = new Vector3(0,0,0);
+		}
+		
+		public void Fly(){
+			int fy = Random.Range(-10,10);
+			ms.setDestination(new Vector3(20,fy,0),speed);
+		}
+	}
+
+
+}
---
+++ hw5/Scripts/UFOFactor.cs
@@ -0,0 +1,53 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using mygame;
+
+public class UFOFactor : MonoBehaviour
+{
+    private List<UFO> used = new List<UFO>();   //正在被使用的飞碟列表
+    private List<UFO> free = new List<UFO>();   //空闲的飞碟列表
+
+    public List <UFO> GetDisk(int round)
+    {
+		Debug.Log("right here UFOFactor 1");
+        int choice = 0;
+        int scope1 = 1, scope2 = 2, scope3 = 3;           //随机的范围
+        float start_y = -10f;                             //刚实例化时的飞碟的竖直位置
+		int ufo_num = Random.Range(1, round + 1);
+		int counts = 0;
+		// 清空used
+		used = new List<UFO>();
+        // 根据回合，选择要飞出的UFO的种类
+		
+		for(int i=0;i<free.Count;i++) {
+			Debug.Log("not empty");
+			int type = Random.Range(1, round+1);
+			if(free[i].type == type) {
+				used.Add(free[i]);
+				free.Remove(free[i]);
+				i --;
+				counts ++;
+				if(counts == ufo_num) 
+					break;
+			}
+		}
+		
+		if(counts < ufo_num){
+			for (int i = 0;i < ufo_num-counts;i ++){
+				int type = Random.Range(1, round);
+				UFO new_ufo = new UFO(type);
+				used.Add(new_ufo);
+			}
+		}
+		return used;
+    }
+
+    // 回收飞碟
+    public void FreeDisk(UFO aUFO)
+    {
+		aUFO.ufo.SetActive(false);
+		aUFO.ufo.transform.position = Vector3.zero;
+		free.Add(aUFO);
+    }
+}
